d748ea1446fb0426afe533fea9772cb321e997a2
Thirunavukkarasu T <97720002+thiru042004@users.noreply.github.com>
2023-09-01 23:49:17 +0530

    FIX. Included mockito framework to mock the parkable interface.

--- src/test/java/com/sixTwoThree/ParkingLotTest.java
+++ src/test/java/com/sixTwoThree/ParkingLotTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.Mockito.mock;
 
 public class ParkingLotTest {
 
@@ -19,12 +20,19 @@ public class ParkingLotTest {
     void beforeEach(){
         parkingLotOne = new ParkingLot(1);
     }
-    Parkable carOne;
-    Parkable carTwo;
+    static Parkable carOne;
+    static Parkable carTwo;
+    /** --@BeforeAll is static
+     *  --because, */
     @BeforeAll
-    void beforeAll(){
-        carOne = new Car();
-        carTwo = new Car();
+    static void beforeAll(){
+        /** --to achieve Fewest Element and Reveals Intention
+         *  --we're using mock() class from mockito framework
+         *  --here, parkable interface is mocked as a class
+         *  --so, we didn't need class car, because car doesn't have any attributes or behaviours*/
+
+        carOne = mock(Parkable.class);
+        carTwo = mock(Parkable.class);
     }
 
     @Test
